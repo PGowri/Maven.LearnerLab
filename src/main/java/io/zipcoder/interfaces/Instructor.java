53615e8b4032293fed4da4ebd2458b633909ae5d
Gowri <gowri@ZipcodersMBP64.local>
2023-12-16 21:22:20 -0500

    completed 4 parts

---
+++ src/main/java/io/zipcoder/interfaces/Instructor.java
@@ -0,0 +1,18 @@
+package io.zipcoder.interfaces;
+
+public class Instructor extends Person implements Teacher{
+
+    public Instructor(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+
+    }
+}
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,35 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void personConstructorTest(){
+        final long id = (long) 1.00;
+        String name = "Bob";
+        Person person = new Person( id, name);
+        long actualId = person.getId();
+        String actualName = person.getName();
+
+        Assert.assertEquals(id, actualId);
+        Assert.assertEquals(name, actualName);
+
+    }
+
+    @Test
+    public void testSetName(){
+        String name = "Leon";
+        Person person = new Person(1,"bob");
+
+        // When
+        person.setName(name);
+        String actual = person.getName();
+
+        // Then
+        Assert.assertEquals(name, actual);
+
+    }
 
 }
